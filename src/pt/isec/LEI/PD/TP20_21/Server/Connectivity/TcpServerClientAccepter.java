93c23677205cc7098e1ef44fac3bfade4d90ccb1
joaovalente99 <57570480+joaovalente99@users.noreply.github.com>
2020-11-26 00:36:07 +0000

    merdas feitas

--- src/pt/isec/LEI/PD/TP20_21/Server/Connectivity/TcpServerClientAccepter.java
+++ src/pt/isec/LEI/PD/TP20_21/Server/Connectivity/TcpServerClientAccepter.java
@@ -1,8 +1,13 @@
 package pt.isec.LEI.PD.TP20_21.Server.Connectivity;
 
 import pt.isec.LEI.PD.TP20_21.Server.Model.Server;
+import pt.isec.LEI.PD.TP20_21.shared.Respostas;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -10,32 +15,65 @@ import java.util.LinkedList;
 /**
  * Faz a connecção inicial do client
  */
-public class TcpServerClientAccepter extends Thread {
-    private int port = -1;
+public class TcpManager {
     private Server server;
+    private int port;
+    private ServerSocket serverSocket;
 
-    public TcpServerClientAccepter(Server server){
+
+    public TcpManager(Server server){
         this.server = server;
-    }
-
-
-
-    public int getPort() {
-        return port;
-    }
-
-    @Override
-    public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket(0);
-            port = serverSocket.getLocalPort();
-            boolean stop = false;
-            while (!stop) {
-                Socket s = serverSocket.accept();
-
-            }
+            serverSocket = new ServerSocket(0);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        port = serverSocket.getLocalPort();
+    }
+
+     static class TcpServerClientAccepter extends Thread {
+        private int port = -1;
+        private boolean stop = false;
+
+
+        //TODO: ver a sincronização de threads e proteção de dados em multithreads
+        public boolean isStop() {
+            return stop;
+        }
+
+        public void setStop(boolean stop) {
+            this.stop = stop;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+
+                while (!stop) {
+                    Socket s = serverSocket.accept();
+                    InputStream iS = s.getInputStream();
+                    OutputStream oS = s.getOutputStream();
+
+                    byte[] bufStr = new byte[256];
+
+                    int nBytes = iS.read(bufStr);
+                    String msgRecebida = new String(bufStr, 0, nBytes);
+                    String msgResposta = "olaaaaaa";
+
+                    bufStr = msgResposta.getBytes();
+                    oS.write(bufStr);
+                    s.close();
+                }
+
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- testes/test.java
+++ testes/test.java
@@ -1,21 +1,42 @@
-import pt.isec.LEI.PD.TP20_21.shared.IpServidor;
-import pt.isec.LEI.PD.TP20_21.shared.Respostas;
-import pt.isec.LEI.PD.TP20_21.shared.Utils;
-
-import java.util.LinkedList;
-import java.util.Objects;
-
 public class test {
     public static void main(String[] args) {
-        LinkedList<IpServidor> ll = new LinkedList<>();
-        Respostas.RUdpClientServerPreConnection resposta = new Respostas.RUdpClientServerPreConnection(6969, ll);
-        System.out.println("size resposta + linkedlist empty:" + Objects.requireNonNull(Utils.objectToBytes(resposta)).length);
-        ll.add(new IpServidor("255.255.255.255", 6969));
-        System.out.println("Size ipservidor:"+ Objects.requireNonNull(Utils.objectToBytes(ll.get(0))).length);
-        System.out.println("size resposta + linkedlist com 1 elemento:"+ Objects.requireNonNull(Utils.objectToBytes(resposta)).length);
-        while(Objects.requireNonNull(Utils.objectToBytes(resposta)).length < 5000){
-            ll.add(new IpServidor("255.255.255.255", 6969));
+//        LinkedList<IpServidor> ll = new LinkedList<>();
+//        Respostas.RUdpClientServerPreConnection resposta = new Respostas.RUdpClientServerPreConnection(6969, ll);
+//        System.out.println("size resposta + linkedlist empty:" + Objects.requireNonNull(Utils.objectToBytes(resposta)).length);
+//        ll.add(new IpServidor("255.255.255.255", 6969));
+//        System.out.println("Size ipservidor:"+ Objects.requireNonNull(Utils.objectToBytes(ll.get(0))).length);
+//        System.out.println("size resposta + linkedlist com 1 elemento:"+ Objects.requireNonNull(Utils.objectToBytes(resposta)).length);
+//        while(Objects.requireNonNull(Utils.objectToBytes(resposta)).length < 5000){
+//            ll.add(new IpServidor("255.255.255.255", 6969));
+//        }
+//        System.out.println("Maximo de ips => " + (ll.size()-1));
+        thread lol = new thread();
+        System.out.println("teste 1");
+        lol.start();
+        System.out.println("teste 2");
+        lol.start();
+        System.out.println("teste 3");
+        lol.start();
+        System.out.println("teste 4");
+        lol.start();
+        System.out.println("teste 5");
+        lol.start();
+    }
+
+    public static class thread extends Thread {
+        public static int conas = 1;
+        @Override
+        public void run() {
+            super.run();
+            int threadnumber = conas++;
+            System.out.println("testing thread "+ threadnumber + "...");
+            try {
+                sleep(60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("testing thread "+ threadnumber + "...");
         }
-        System.out.println("Maximo de ips => " + (ll.size()-1));
     }
 }
