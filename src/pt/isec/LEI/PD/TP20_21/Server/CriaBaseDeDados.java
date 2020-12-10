ff5943b71d7e7334f889ac903d5e896aef60036f
joaovalente99 <57570480+joaovalente99@users.noreply.github.com>
2020-12-10 01:29:30 +0000

    Revert "base de dados"
    This reverts commit 54ebf8180cf95c5014548d3408298880abb9b45a.

--- src/pt/isec/LEI/PD/TP20_21/Server/CriaBaseDeDados.java
+++ src/pt/isec/LEI/PD/TP20_21/Server/CriaBaseDeDados.java
@@ -54,8 +54,8 @@ public class CriaBaseDeDados {
     void sql(int i, boolean resetar) {
 
         try {
-            stmt.execute("CREATE DATABASE IF NOT EXISTS messager_db_" + i + ";");
-            stmt.execute("USE messager_db_" + i + ";");
+            stmt.execute("CREATE DATABASE IF NOT EXISTS messager_db_"+i+";");
+            stmt.execute("USE messager_db_"+i+";");
             if (false) {
                 stmt.execute(
                         "DROP TABLE IF EXISTS mensagens;\n" +
@@ -63,61 +63,50 @@ public class CriaBaseDeDados {
                                 "DROP TABLE IF EXISTS utilizadores;\n" +
                                 "drop table if exists ficheiros;\n");
             }
-            stmt.execute("create table if not exists canais\n" +
-                    "(\n" +
-                    "    id int not NULL unique auto_increment,\n" +
-                    "    nome        text not null,\n" +
-                    "    descricao   text,\n" +
-                    "    primary key (id)\n" +
-                    ");\n" +
-                    "\n" +
-                    "\n");
+            stmt.execute("create table if not exists ficheiros" +
+                    "(" +
+                    "    id   int  not null auto_increment unique," +
+                    "    nome text not null," +
+                    "    PRIMARY KEY (id)" +
+                    ");");
             stmt.execute("create table if not exists utilizadores\n" +
                     "(\n" +
-                    "    id       int         not null auto_increment unique,\n" +
-                    "    username varchar(64) not null unique,\n" +
-                    "    nome     text        not null,\n" +
-                    "    hash     text        not null,\n" +
-                    "    PRIMARY KEY (id)\n" +
-                    ");\n" +
-                    "\n");
-            stmt.execute("create table if not exists canaisGrupo\n" +
+                    "    id           int         not null auto_increment unique,\n" +
+                    "    username     varchar(50) not null unique,\n" +
+                    "    hash         text        not null,\n" +
+                    "    profilepicId int         not null default 1,\n" +
+                    "    PRIMARY KEY (id),\n" +
+                    "    FOREIGN KEY (profilepicId) REFERENCES ficheiros (id)\n" +
+                    ");");
+                    stmt.execute( "create table if not exists canais\n" +
                     "(\n" +
-                    "    canal_id    int not null,\n" +
+                    "    id          int  not null auto_increment unique,\n" +
+                    "    nome        text not null,\n" +
+                    "    descricao   text,\n" +
                     "    password    text not null,\n" +
                     "    moderadorId int  not null,\n" +
-                    "    foreign key (canal_id) references canais (id),\n" +
+                    "    PRIMARY KEY (id),\n" +
                     "    foreign key (moderadorId) references utilizadores (id)\n" +
-                    ");\n" +
-                    "\n");
-            stmt.execute("create table if not exists canaisDM\n" +
-                    "(\n" +
-                    "    canal_id    int not null,\n" +
-                    "    pessoa1   int  not null,\n" +
-                    "    pessoa2   int  not null,\n" +
-                    "    foreign key (canal_id) references canais (id),\n" +
-                    "    foreign key (pessoa1) references utilizadores (id),\n" +
-                    "    foreign key (pessoa2) references utilizadores (id)\n" +
-                    ");\n" +
-                    "\n");
-            stmt.execute("create table if not exists mensagens\n" +
-                    "(\n" +
+                    ");\n" );
+                    stmt.execute("create table if not exists mensagens(\n"+
                     "    id            int                                 not null auto_increment unique,\n" +
                     "    dataHoraEnvio timestamp DEFAULT CURRENT_TIMESTAMP not null,\n" +
                     "    authorId      int                                 not null,\n" +
                     "    canalId       int                                 not null,\n" +
                     "    isAFile       bool                                not null,\n" +
-                    "    mensagem      varchar(255),\n" +
+                    "    mensagem      text,\n" +
+                    "    fileId        int,\n" +
                     "    PRIMARY KEY (id),\n" +
                     "    FOREIGN KEY (authorId) REFERENCES utilizadores (id),\n" +
-                    "    foreign key (canalId) references canais (id)\n" +
+                    "    foreign key (canalId) references canais (id),\n" +
+                    "    foreign key (fileId) references ficheiros(id)\n" +
                     ");");
-            stmt.execute("CREATE USER IF NOT EXISTS `server_" + i + "`@`localhost` IDENTIFIED BY 'W-pass-123';\n");
-            stmt.execute("GRANT SELECT, INSERT, UPDATE, DELETE ON * TO `server_" + i + "`@`localhost`;\n");
+                    stmt.execute("CREATE USER IF NOT EXISTS `server_"+i+"`@`localhost` IDENTIFIED BY 'W-pass-123';\n" );
+                    stmt.execute("GRANT SELECT, INSERT, UPDATE, DELETE ON * TO `server_"+i+"`@`localhost`;\n" );
 
-            stmt.execute("FLUSH PRIVILEGES;");
+                    stmt.execute("FLUSH PRIVILEGES;" );
 
-            stmt.executeQuery("COMMIT;");
+                   stmt.executeQuery( "COMMIT;");
 
         } catch (SQLException e) {
             e.printStackTrace();
